8127549fc90bd832e2d65497425fa38a8898e13d
smile dog <simonneartstuff@gmail.com>
2022-12-16 12:14:31 -0500

    Update homescript.js

--- homepage/homescript.js
+++ homepage/homescript.js
@@ -1,12 +1,22 @@
-var images = [
-	 "https://cinni.neocities.org/images/creatures/cats/cute_cat6.gif",
-  "https://cinni.neocities.org/images/creatures/cats/cute_cat4.gif",
-  "https://cinni.neocities.org/images/creatures/cats/cute_cat2.gif"
-	];
+var imlocation = "advert/";
+var currentdate = 0;
+var image_mumber = 0;
+function ImageArray (n) {
+	this.length = n;
+	for (var i =1; i <= n; i++) {
+	this[i] = ' '
+	}
 	
-var randomImage = images[Math.floor(Math.random() * images.length)];
-console.log(randomImage);
-
-var image = "<img src='" + randomImage + "'>";
-
-document.getElementById("img-load").innerHTML = image;
+}
+image = new ImageArray(13)
+image[0] = 'brawl.png'
+image[1] = 'doll.png'
+image[2] = 'pizza.png'
+var rand = 60/image.length
+function randomimage() {
+	currentdate = new Date()
+	image_number = current.date.getSeconds()
+	image_number = Math.floor(image_number/rand)
+	return(image[image_number])
+}
+document.write("img src'" + imlocation + randomimage()+ "'>");
