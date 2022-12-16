bf12029635cc33c99fe2941bafd297a3802d7d25
smile dog <simonneartstuff@gmail.com>
2022-12-16 11:41:34 -0500

    random image attempt 2

--- homepage/homescript.js
+++ homepage/homescript.js
@@ -1,7 +1,8 @@
-window.onload = choosePic;
-
-var myPix = new Array("advert/brawl.png", "advert/doll.png", "advert/pizza.png");
-
-function choosePic() {
-		var randomNum = Math.floor(Math.random() * myPix.length);
-document.get.ElementById("myPicture").src= myPix[randomNum];
+var description = [
+	"advert/doll.png",
+	"advert/brawl.png",
+	"advert/pizza.png"
+	];
+	
+var size = description.length
+var x = Math.floor(size*Math.random())
