b98f9da2f0d27f71cf32b63ce90553484e2ffea4
smile dog <simonneartstuff@gmail.com>
2022-12-16 11:52:57 -0500

    attempt 4

--- homepage/homescript.js
+++ homepage/homescript.js
@@ -1,12 +1,12 @@
 var images = [
-	"advert/doll.png",
-	"advert/brawl.png",
-	"advert/pizza.png"
+	 "https://cinni.neocities.org/images/creatures/cats/cute_cat6.gif",
+  "https://cinni.neocities.org/images/creatures/cats/cute_cat4.gif",
+  "https://cinni.neocities.org/images/creatures/cats/cute_cat2.gif"
 	];
 	
 var randomImage = images[Math.floor(Math.random() * images.length)];
 console.log(randomImage);
 
-var image = "<img src= '" + randomImage + "'>";
+var image = "<img src='" + randomImage + "'>";
 
 document.getElementById("img-load").innerHTML = image;
